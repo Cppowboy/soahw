320791989ff0b13d9430dcbc62ad26cad11f279d
cppowboy <panyx93@163.com>
2015-03-30 22:33:30 +0800

    use requests

--- soahw/views.py
+++ soahw/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-import socket,urllib,urllib2,httplib,json
+import requests
 
 client_id='3223538172'
 client_secret='b73de5b91fdfeb12e7201547a41bcd48'
@@ -17,33 +17,13 @@ def users(request):
     return HttpResponse("Users Page")
 
 def access(request):
-    '''
-    if request.GET.has_key('code'):
-        data={'client_id':client_id, 'client_secret':client_secret,\
-                 'grant_type':'authorization_code', 'redirect_uri':'http://101.200.81.62/access/',\
-                 'code':request.GET['code']}
-        host='api.weibo.com'
-        url='/oauth2/access_token'
-        para=urllib.urlencode(data)
-        headers={'Content-type':'application/x-www-form-urlencoded','Accept':'text/plain'}
-        addr=socket.gethostbyname(host)
-        con=httplib.HTTPConnection(addr)
-        con.request('POST',url,para,headers)
-        response=con.getresponse()
-        return HttpResponse(response.status)
-    else:
-        return HttpResponse('can not find code')
-    '''
     if request.GET.has_key('code'):
         code=request.GET['code']
         data={'grant_type':'authorization_code', 'redirect_uri':'http://101.200.81.62/access','code':code}
         url='https://api.weibo.com/oauth2/access_token'
-        para=urllib.urlencode(data)
-        req=urllib2.Request(url,para)
-        req.add_header('client_id',client_id)
-        req.add_header('client_secret',client_secret)
-        resp = urllib2.urlopen(req, timeout=5)
-        return HttpResponse(resp.read())
+        headers={'client_id':client_id,'client_secret':client_secret)
+        resp=requests.post(url,data=data,headers=headers)
+        return HttpResponse(resp.json())
     else:
         return HttpResponse('can not find code')
 
