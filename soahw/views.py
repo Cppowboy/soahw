595ccff65bb8b402f0ac9bcff458dc779a4ef82b
cppowboy <panyx93@163.com>
2015-03-30 15:17:21 +0800

    use httplib to make post

--- soahw/views.py
+++ soahw/views.py
@@ -1,7 +1,7 @@
 #coding=utf8
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-import socket,urllib,httplib,json
+import socket,urllib,urllib2,httplib,json
 
 client_id='3223538172'
 client_secret='b73de5b91fdfeb12e7201547a41bcd48'
@@ -22,15 +22,16 @@ def access(request):
         data={'client_id':client_id, 'client_secret':client_secret,\
                  'grant_type':'authorization_code', 'redirect_uri':'http://101.200.81.62/access/',\
                  'code':request.GET['code']}
-        burl='api.weibo.com'
-        surl="/oauth2/access_token"
+        host='https://api.weibo.com'
+        url='/oauth2/access_token'
         para=urllib.urlencode(data)
-        #headers={"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1) Gecko/20090624 Firefox/3.5","Accept": "text/json"}
-        addr=socket.gethostbyname(burl)
-        connection=httplib.HTTPConnection(addr,80,timeout=3)
-        connection.request('post',surl,body=para)
-        response=connection.getresponse()
-        return HttpResponse(response.read())
+        headers={'Content-Type':'application/x-www-form-urlencoded','Connection':'Keep-Alive'}
+        try:
+            addr=socket.gethostbyname(host)
+            con=httplib.HTTPConnection(addr,80,timeout=3)
+            con.request('POST',url,para,headers)
+            response=con.getresponse()
+        return HttpResponse(response.status)
     else:
         return HttpResponse('can not find code')
 
